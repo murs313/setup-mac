0dfede66a8deb46ffa76a9286cb6e0b767cdb687
murs313 <murbook313@gmail.com>
2022-06-30 17:17:37 +0900

    Minor changes

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
+# フォルダを英語表記にする
+rm ~/*/.localized
+sudo rm /*/.localized
 # Finderで不可視ファイルを見れるようにする
 defaults write com.apple.finder AppleShowAllFiles TRUE
 # Finderをフルパス表示にする
 defaults write com.apple.finder _FXShowPosixPathInTitle -boolean true
-# フォルダを英語表記にする
-rm ~/*/.localized
-sudo rm /*/.localized
 # Finderを再起動する
 killall Finder
 # スクリーンショットをダウンロードに保存するようにする
@@ -12,24 +12,25 @@ defaults write com.apple.screencapture location ~/Downloads/
 # スクリーンショットの影を消す
 defaults write com.apple.screencapture disable-shadow -boolean true
 
-# Homebrew
+# Install Homebrew
 # 最初にパスワードが必要。30分くらい。
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-# git
+# Set up git
 brew install git
 git config --global user.name "murs313"
 git config --global user.email "murbook313@gmail.com"
 
-git clone https://github.com/murs313/setup-mac.git Projects/setup-mac
-
-# karabiner
-brew install --cask karabiner-elements
-cp -r ~/Projects/setup-mac/karabiner ~/.config
+# Clone repo
+git clone https://github.com/murs313/setup-mac.git ~/Git/setup-mac
 
 # zsh
 cp -r ~/Projects/setup-mac/.zshrc ~/.zshrc
 source ~/.zshrc
+
+# karabiner
+brew install --cask karabiner-elements
+cp -r ~/Git/setup-mac/karabiner ~/.config
 
 # essentials
 brew install --cask evernote
