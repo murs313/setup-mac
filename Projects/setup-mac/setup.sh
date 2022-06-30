833be4f4a2f47991ed8001e47a9393701bd4e4e8
murs313 <murbook313@gmail.com>
2022-06-30 16:00:43 +0900

    Try home directory

--- Projects/setup-mac/setup.sh
+++ Projects/setup-mac/setup.sh
@@ -1,11 +1,10 @@
-echo "Setting up defaults ..."
+# フォルダを英語表記にする
+rm ~/*/.localized
+sudo rm /*/.localized
 # Finderで不可視ファイルを見れるようにする
 defaults write com.apple.finder AppleShowAllFiles TRUE
 # Finderをフルパス表示にする
 defaults write com.apple.finder _FXShowPosixPathInTitle -boolean true
-# フォルダを英語表記にする
-rm ~/*/.localized
-sudo rm /*/.localized
 # Finderを再起動する
 killall Finder
 # スクリーンショットをダウンロードに保存するようにする
@@ -13,50 +12,51 @@ defaults write com.apple.screencapture location ~/Downloads/
 # スクリーンショットの影を消す
 defaults write com.apple.screencapture disable-shadow -boolean true
 
+# Install Homebrew
 # 最初にパスワードが必要。30分くらい。
-if ! command_exists brew ; then
-echo "Setting up Homebrew ..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew -v
-fi
 
-if ! command_exists git ; then
-echo "Setting up git ..."
+# Set up git
 brew install git
 git --version
 git config --global user.name "murs313"
 git config --global user.email "murbook313@gmail.com"
-fi
 
-git clone https://github.com/murs313/setup-mac.git Projects/setup-mac
-
-echo "Setting up Karabiner ..."
-brew install --cask karabiner-elements
-cp -r ~/Projects/setup-mac/karabiner ~/.config
-
-echo "Setting up zsh ..."
-cp -r ~/Projects/setup-mac/.zshrc ~/.zshrc
+# Set configuraion file
+git clone https://github.com/murs313/setup-mac.git ~
 source ~/.zshrc
 
-echo "brew installing ..."
+# brew install
+brew install --cask karabiner-elements
 brew install --cask evernote
-brew install --cask slack
 brew install --cask google-chrome
+brew install --cask slack
 brew install --cask discord
 brew install --cask itsycal
-brew install --cask clipy
 brew install --cask visual-studio-code
+brew install --cask tableplus
 brew install --cask toggl-track
+brew install --cask clipy
+brew install --cask appcleaner
 
-# Android
-# brew install --cask android-studio
+# 遊び
+brew install --cask streamlabs-obs
+brew install --cask steam
+
+# 仕事
+brew install --cask 1password
+brew install direnv
+brew install rbenv
 
 # Backend
-# brew install --cask docker
-# brew install --cask datagrip
-# brew install --cask postman
-# brew install jq
-# brew install wget
+brew install --cask docker
+brew install --cask postman
+brew install jq
+brew install wget
 
 # Frontend
-# brew install npm
+brew install npm
+
+# Android
+brew install --cask android-studio
